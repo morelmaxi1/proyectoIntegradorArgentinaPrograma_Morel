022a33911518399e04ec2d04c6168fab1804f1cc
morelmaxi1 <morelmaximiliano29@gmail.com>
2023-01-06 10:21:02 -0300

    inicioPortafolio

--- proyectoIntegradorArgentinaPrograma/src/app/app.module.ts
+++ proyectoIntegradorArgentinaPrograma/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannersComponent } from './components/banners/banners.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannersComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
